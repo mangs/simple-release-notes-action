91e76e5280107993f25b7c205eeb2452f8900ff3
Eric L. Goldstein <egoldstein@babbel.com>
2022-03-20 18:42:31 -0400

    ready to test a release note creation on CI

--- src/marked.mjs
+++ src/marked.mjs
@@ -1,17 +1,21 @@
+#!/usr/bin/env node
+
 // External Imports
 import { marked } from 'marked';
+import { Octokit } from '@octokit/action';
 import { readFile } from 'node:fs/promises';
-// import { Octokit } from '@octokit/action';
+
+// Local Variables
+const tagPrefix = 'v';
+const changelogPath = './CHANGELOG.md';
 
 // Local Functions
 async function main() {
-  // Figure out where the changelog file is
-  const changelogPath = './CHANGELOG.md';
+  // Get the target version from package.json
+  const packageJsonContents = await readFile('./package.json', 'utf8');
+  const { version: targetVersion } = JSON.parse(packageJsonContents);
 
-  // Figure out the target version
-  const targetVersion = '1.0.0';
-
-  // Get the file contents
+  // Read the changelog contents, then parse it into tokens
   const fileContents = await readFile(changelogPath, 'utf8');
   const tokens = marked.lexer(fileContents);
 
@@ -32,23 +36,43 @@ async function main() {
     }
   }
 
-  // Compile the raw markdown output, then send it to GitHub
-  const rawCombinedMarkdown = tokensForOutput.map((token) => token.raw).join('');
-  // console.log('NODES', tokensForOutput);
-  console.log(rawCombinedMarkdown);
+  // Store the tokens' combined markdown
+  const combinedMarkdown = tokensForOutput
+    .map((token) => token.raw)
+    .join('')
+    .trim();
+  console.log('RAW MARKDOWN');
+  console.log(combinedMarkdown);
 
-  // Connect to GitHub
+  // Collect environment metadata
   const repositoryMetadata = process.env.GITHUB_REPOSITORY.split('/');
   const [repoOwner, repoName] = repositoryMetadata;
+  const tagName = `${tagPrefix}${targetVersion}`;
+  const commitHash = process.env.GITHUB_SHA;
+  // const isDraft = false;
+  // const isPrerelease = false;
   console.log('GITHUB');
   console.log(`OWNER "${repoOwner}"`);
   console.log(`REPO "${repoName}"`);
-  // const octokit = new Octokit();
-  // await octokit.request('POST /repos/{owner}/{repo}/releases', {
-  //   owner: 'octocat',
-  //   repo: 'hello-world',
-  //   tag_name: 'tag_name',
-  // });
+  console.log(`TAG "${tagName}"`);
+  console.log(`COMMIT HASH "${commitHash}"`);
+
+  // Authenticate on Github, then create a release note
+  if (process.env.LOCAL) {
+    process.exit(); // Don't make any requests to GitHub when doing local testing
+  }
+  const octokit = new Octokit();
+  await octokit.request(`POST /repos/${repoOwner}/${repoName}/releases`, {
+    body: combinedMarkdown,
+    // draft: isDraft,
+    generate_release_notes: false,
+    name: targetVersion,
+    owner: repoOwner,
+    // prerelease: isPrerelease,
+    repo: repoName,
+    tag_name: tagName,
+    target_commitish: commitHash,
+  });
 }
 
 // Begin Execution
