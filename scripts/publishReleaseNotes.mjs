2a419100b06c1e9fe4eeac33e599e1fd3a2485a5
Eric L. Goldstein <egoldstein@babbel.com>
2022-03-20 22:48:35 -0400

    Slight script refactor, enable generate_release_notes

--- scripts/publishReleaseNotes.mjs
+++ scripts/publishReleaseNotes.mjs
@@ -6,27 +6,27 @@ import { Octokit } from '@octokit/action';
 import { readFile } from 'node:fs/promises';
 
 // Local Variables
-const tagPrefix = 'v';
 const changelogPath = './CHANGELOG.md';
+const packageJsonPath = './package.json';
+const tagPrefix = 'v';
 
 // Local Functions
 async function main() {
   // Get the target version from package.json
-  const packageJsonContents = await readFile('./package.json', 'utf8');
+  const packageJsonContents = await readFile(packageJsonPath, 'utf8');
   const { version: targetVersion } = JSON.parse(packageJsonContents);
 
   // Read the changelog contents, then parse it into tokens
   const fileContents = await readFile(changelogPath, 'utf8');
-  const tokens = marked.lexer(fileContents);
+  const markdownTokens = marked.lexer(fileContents);
 
   // Store the markdown nodes between the matched, target version number heading and the following heading at the same depth
   let isCapturing = false;
   let targetNodeDepth = 0;
-  const targetNodeType = 'heading';
   const tokensForOutput = [];
-  for (const token of tokens) {
+  for (const token of markdownTokens) {
     if (isCapturing) {
-      if (token.type === targetNodeType && token.depth === targetNodeDepth) {
+      if (token.type === 'heading' && token.depth === targetNodeDepth) {
         break;
       }
       tokensForOutput.push(token);
@@ -35,6 +35,12 @@ async function main() {
       targetNodeDepth = token.depth;
     }
   }
+  if (!isCapturing) {
+    console.log(
+      `No match for version "${targetVersion}" found in changelog file "${changelogPath}"`,
+    );
+    process.exit(1);
+  }
 
   // Store the tokens' combined markdown
   const combinedMarkdown = tokensForOutput
@@ -42,7 +48,7 @@ async function main() {
     .join('')
     .trim();
 
-  // Collect environment metadata
+  // Prepare metadata
   const repositoryMetadata = process.env.GITHUB_REPOSITORY.split('/');
   const [repoOwner, repoName] = repositoryMetadata;
   const tagName = `${tagPrefix}${targetVersion}`;
@@ -58,7 +64,7 @@ async function main() {
   await octokit.request(`POST /repos/${repoOwner}/${repoName}/releases`, {
     body: combinedMarkdown,
     // draft: isDraft,
-    generate_release_notes: false,
+    generate_release_notes: true,
     name: targetVersion,
     owner: repoOwner,
     // prerelease: isPrerelease,
