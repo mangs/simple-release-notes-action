6033101d15cdc636a12638ea3632dff71f46552e
Eric L. Goldstein <mangs@users.noreply.github.com>
2022-03-21 00:13:49 -0400

    test octokit (#2)

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
-# simple-release-notes-action
+# `mangs/simple-release-notes-action`
 
 GitHub Action that auto-publishes release notes using a very simple-to-follow set of rules
+
+## Rules for Use
+
+As its name implies, this action is intended to be easy to use. Here are the rules to follow for using this action:
+
+- When this action runs, it uses the `version` field in your `package.json` to find the relevant "section" in your changelog; this is the target version number
+- Your changelog file is structured into version number "sections" as follows, "section" order does not matter (use [this repository's changelog](./CHANGELOG.md) as an example):
+
+```markdown
+# <changelog title>
+
+## <version number>
+
+<any markdown can go here>
+
+## <another version number>
+
+<any markdown can go here>
+
+etc...
+```
+
+- A new release will be created automatically as follows:
+  - Title will be the target version number (a.k.a the `version` field in your `package.json`)
+  - Description will be the matched "section" of markdown
+  - Tag will be the concatenation of the tag prefix and the target version number, in that order
+- Use in a Linux environment; this is the default (this action was not tested with [macOS or Windows runners](https://docs.github.com/en/actions/using-github-hosted-runners/about-github-hosted-runners#supported-runners-and-hardware-resources), but it may function just fine)
+
+## Action Inputs
+
+| Name               | Required | Default Value    | Descripition                                                                                                            |
+| ------------------ | -------- | ---------------- | ----------------------------------------------------------------------------------------------------------------------- |
+| `changelog_path`   | N        | `./CHANGELOG.md` | Path to the target changelog file                                                                                       |
+| `packagejson_path` | N        | `./package.json` | Path to the target `package.json` file                                                                                  |
+| `tag_prefix`       | N        | `v`              | Prefix used to create the tag - the concatenation of this value and the version number from package.json, in that order |
---
+++ scripts/publishReleaseNotes.mjs
@@ -0,0 +1,77 @@
+#!/usr/bin/env node
+
+// External Imports
+import { marked } from 'marked';
+import { Octokit } from '@octokit/action';
+import { readFile } from 'node:fs/promises';
+
+// Local Variables
+const changelogPath = process.env.INPUT_CHANGELOG_PATH ?? './CHANGELOG.md';
+const packageJsonPath = process.env.INPUT_PACKAGEJSON_PATH ?? './package.json';
+const tagPrefix = process.env.INPUT_TAG_PREFIX ?? 'v';
+
+// Local Functions
+async function main() {
+  // Get the target version from package.json
+  const packageJsonContents = await readFile(packageJsonPath, 'utf8');
+  const { version: targetVersion } = JSON.parse(packageJsonContents);
+
+  // Read the changelog contents, then parse it into tokens
+  const fileContents = await readFile(changelogPath, 'utf8');
+  const markdownTokens = marked.lexer(fileContents);
+
+  // Store the markdown nodes between the matched, target version number heading and the following heading at the same depth
+  let isCapturing = false;
+  let targetNodeDepth = 0;
+  const tokensForOutput = [];
+  for (const token of markdownTokens) {
+    if (isCapturing) {
+      if (token.type === 'heading' && token.depth === targetNodeDepth) {
+        break;
+      }
+      tokensForOutput.push(token);
+    } else if (token.type === 'heading' && token.text === targetVersion) {
+      isCapturing = true;
+      targetNodeDepth = token.depth;
+    }
+  }
+  if (!isCapturing) {
+    console.log(
+      `No match for version "${targetVersion}" found in changelog file "${changelogPath}"`,
+    );
+    process.exit(1);
+  }
+
+  // Store the tokens' combined markdown
+  const combinedMarkdown = tokensForOutput
+    .map((token) => token.raw)
+    .join('')
+    .trim();
+
+  // Prepare metadata
+  const repositoryMetadata = process.env.GITHUB_REPOSITORY.split('/');
+  const [repoOwner, repoName] = repositoryMetadata;
+  const tagName = `${tagPrefix}${targetVersion}`;
+  const commitHash = process.env.GITHUB_SHA;
+  // const isDraft = false;
+  // const isPrerelease = false;
+
+  // Authenticate on Github, then create a release note
+  if (process.env.CI !== 'true') {
+    process.exit(); // Don't make any requests to GitHub when doing local testing
+  }
+  const octokit = new Octokit();
+  await octokit.request(`POST /repos/${repoOwner}/${repoName}/releases`, {
+    body: combinedMarkdown,
+    // draft: isDraft,
+    name: targetVersion,
+    owner: repoOwner,
+    // prerelease: isPrerelease,
+    repo: repoName,
+    tag_name: tagName,
+    target_commitish: commitHash,
+  });
+}
+
+// Begin Execution
+main();
